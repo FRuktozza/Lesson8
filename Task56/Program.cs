81c4711d91c07fd7bf8c4140680e0ccca455637b
FRuktozza <121963443+FRuktozza@users.noreply.github.com>
2023-01-22 18:50:15 +0300

    Add files via upload

---
+++ Task56/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите размерность прямоугольного массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[n,n];
+int row = -1;
+int maxSum = Int32.MinValue;
+int currSum;
+
+for (int i = 0; i < n; i++)
+{
+    currSum = 0;
+    Console.WriteLine($"Вводите элементы строки {i+1}");
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = Convert.ToInt32(Console.ReadLine());
+        currSum += array[i, j];
+    }
+
+    if (currSum >= maxSum)
+    {
+        maxSum = currSum;
+        row = i;
+    }
+}
+
+Console.WriteLine($"Максимальная сумма в строке {row+1}");
